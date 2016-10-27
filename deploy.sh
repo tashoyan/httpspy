2bd132874a2c2fb4b9600851f4914d2199cdc3d2
tashoyan <tashoyan@gmail.com>
2016-10-27 17:03:16 +0300

    Store Maven settings to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
 mvn clean deploy -P release
+
+#<settings>
+#  <profiles>
+#    <profile>
+#      <id>ossrh</id>
+#      <activation>
+#        <activeByDefault>true</activeByDefault>
+#      </activation>
+#      <properties>
+#        <gpg.executable>gpg2</gpg.executable>
+#        <gpg.passphrase>my_passphrase</gpg.passphrase>
+#      </properties>
+#    </profile>
+#  </profiles>
+#  <servers>
+#    <server>
+#      <id>ossrh</id>
+#      <username>my_username</username>
+#      <password>my_password</password>
+#    </server>
+#  </servers>
+#</settings>
